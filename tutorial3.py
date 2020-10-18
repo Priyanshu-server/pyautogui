57beff32d449932d838acb2fe316a1cdd476eb1f
Priyanshu <69322912+Priyanshu-server@users.noreply.github.com>
2020-10-18 23:14:13 +0530

    Add files via upload

---
+++ tutorial3.py
@@ -0,0 +1,15 @@
+import pyautogui
+pyautogui.FAILSAFE = True
+
+# pyautogui.moveTo(27,376,2,pyautogui.easeInQuad) #start slow , end fast
+# pyautogui.moveTo(27,376,2,pyautogui.easeOutQuad) #start fast , end slow
+# pyautogui.moveTo(27,376,2,pyautogui.easeInOutQuad) #start fast , end slow
+# pyautogui.moveTo(27,376,2,pyautogui.easeInBounce) #start fast , end slow
+# pyautogui.moveTo(27,376,2,pyautogui.easeInElastic) #start fast , end slow
+
+
+# pyautogui.click(555,39)
+# pyautogui.click(187,339,button='right')
+# pyautogui.click(335,318,button='left',clicks = 2,interval = 0.2)
+# pyautogui.doubleClick(335,318)
+# pyautogui.tripleClick(335,318)
