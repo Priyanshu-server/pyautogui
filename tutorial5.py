8b722ae51879213d1c46a59ff27185539f72579a
Priyanshu <69322912+Priyanshu-server@users.noreply.github.com>
2020-10-27 23:09:59 +0530

    Add files via upload

---
+++ tutorial5.py
@@ -0,0 +1,17 @@
+import pyautogui,time
+
+time.sleep(2)
+
+# pyautogui.write('Hello World !',interval=0.2)
+
+# pyautogui.press('left')
+# pyautogui.press('enter')
+
+'''
+pyautogui.keyDown('alt')
+pyautogui.press('tab')
+pyautogui.keyUp('alt')
+'''
+
+# pyautogui.press(['left','left','left',])
+# pyautogui.press('left',presses=3)
