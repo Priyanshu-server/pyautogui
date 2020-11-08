991e1ab0ea08cf9dd5745fb6244451728acf8c84
Priyanshu <69322912+Priyanshu-server@users.noreply.github.com>
2020-11-08 17:13:01 +0530

    Add files via upload

---
+++ tutorial8.py
@@ -0,0 +1,9 @@
+import pyautogui,time
+#GreyScale (30% faster)
+# x  = pyautogui.locateOnScreen("task.png", grayscale = True,confidence = 0.7)
+# time.sleep(2)
+# im  = pyautogui.screenshot()
+# print(im.getpixel((100,200)))
+# print(pyautogui.pixel(100,200))
+
+# print(pyautogui.pixelMatchesColor(100,200,(40, 42, 65),tolerance=10))
